d6840e328c4ec4756b979f3a17f5c9e63b7fd45d
CipherX52 <32170371+CipherX52@users.noreply.github.com>
2021-11-26 21:49:51 +0530

    login finalized, touch up on todo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// let form = document.getElementById("loginform");
-// form.addEventListener("submit",validate);
+let form = document.getElementById("loginform")
+form.addEventListener("submit",validate);
 document.getElementById("submitBtn").addEventListener("click",validate);
 
-function validate(){
+function validate(event){
     init();
+    event.preventDefault();
 
     let username = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
@@ -11,24 +12,17 @@ function validate(){
     if (username.value.trim() == "admin" && password.value == "12345"){
         username.classList.add("is-valid");
         password.classList.add("is-valid");
-        // window.setTimeout(redirect,300);
-        return true;
+        document.getElementById("loginform").submit();
     }
     else if (username.value.trim() == "admin" && password.value != "12345"){
         wrongPass();
         username.classList.add("is-valid");
-        return false;
     }
     else{
         wrongName();
         wrongPass();
-        return false;
     }
 
-}
-
-function redirect(){
-    window.location = "./todo.html";
 }
 
 function wrongName(){
